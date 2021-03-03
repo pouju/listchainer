dbda70ee3af2b1e1931a593f639488aee9c1e10c
immonel2 <leo.immonen@aalto.fi>
2021-03-03 21:42:36 +0200

    Added info text for empty list

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 import ItemCard from './ItemCard'
 import activities from '../activities.json'
+import {
+  Typography
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: 50
+  },
+  text: {
+    alignSelf: 'center'
+  }
+}))
+
+const EmptyListText = () => {
+  const classes = useStyles()
+  return (
+    <Typography variant="h4" className={classes.text}>
+      No items
+    </Typography>
+  )
+}
 
 const ItemList = ({ selected }) => {
+  const classes = useStyles()
 
   const items = 
     activities
@@ -12,10 +37,14 @@ const ItemList = ({ selected }) => {
       .sort((a, b) => b.packs - a.packs)
 
   return (
-    <div className="item-list">
-      {items.map((item, i) =>
-        <ItemCard item={item} key={i} />
-      )}
+    <div className={classes.root}>
+      {
+        !items.length
+        ? <EmptyListText />
+        : items.map((item, i) =>
+            <ItemCard item={item} key={i} />
+          )
+      }
     </div>
   )
 }
