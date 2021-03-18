3e03e4f437bda0134137bbd992d9b09c50fe1642
Juho Pousi <juho.pousi@aalto.fi>
2021-03-18 14:54:15 +0200

    add favicon ico, change title, add more colors

--- src/utils.js
+++ src/utils.js
@@ -7,7 +7,42 @@ const colors = [
   '#FFC27B',  // ORANGE
   '#7BFFBF',  // LIGHT GREEN
   '#7B80FF',  // INDIGO
-  '#EC7BFF'   // PURPLE
+  '#EC7BFF',  // PURPLE
+  '#0048BA',
+  '#7CB9E8',
+  '#B284BE',
+  '#EFDECD',
+  '#E52B50',
+  '#3B7A57',
+  '#FF7E00',
+  '#9966CC',
+  '#A4C639',
+  '#915C83',
+  '#008000',
+  '#00FFFF',
+  '#7FFFD4',
+  '#D0FF14',
+  '#FF9966',
+  '#568203',
+  '#89CFF0',
+  '#F4C2C2',
+  '#FF91AF',
+  '#848482',
+  '#F5F5DC',
+  '#3D2B1F',
+  '#54626F',
+  '#ACE5EE',
+  '#1F75FE',
+  '#CD7F32',
+  '#C95A49',
+  '#E23D28',
+  '#9FA91F',
+  '#D2691E',
+  '#F88379',
+  '#8C92AC',
+  '#FBEC5D',
+  '#FFBCD9',
+  '#00FFFF',
 ]
 
 const generateColor = (activityName) => {
