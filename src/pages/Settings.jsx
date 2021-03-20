8e5bd07f2823203e4c7444e9b8bb83ca991a63c2
immonel2 <leo.immonen@aalto.fi>
2021-03-21 00:13:22 +0200

    Added a dialog for reset button

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,29 +1,70 @@
-import React from 'react'
-import { Button, Container, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Button, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Typography } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
+import Alert from '../components/Alert'
 
 const useStyles = makeStyles(() => ({
+  title: {
+    paddingBottom: '40px'
+  },
   buttons: {
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 20
   }
 }))
 
-const resetCache = () => window.localStorage.clear()
+const ResetConfirmationDialog = ({ open, action, cancel }) => (
+  <Dialog open={open} onClose={cancel}>
+    <DialogTitle>Are you sure?</DialogTitle>
+    <DialogContent>
+      <DialogContentText>
+        This will clear all your changes and load preset activities.
+        Do you want to continue?
+      </DialogContentText>
+    </DialogContent>
+    <DialogActions>
+      <Button autoFocus onClick={cancel}>No</Button>
+      <Button onClick={action}>Yes</Button>
+    </DialogActions>
+  </Dialog>
+)
 
 const Settings = () => {
   const classes = useStyles()
+  const [ showAlert, setShowAlert ] = useState(false)
+  const [ dialogOpen, setDialogOpen ] = useState(false)
+
+  const resetCache = () => {
+    window.localStorage.clear()
+    setDialogOpen(false)
+    setShowAlert(true)
+  }
 
   return (
-    <div >
-      <Typography variant="h4">
-        Settings Page
+    <div>
+      <Typography variant="h4" className={classes.title}>
+        Settings
       </Typography>
       <Container className={classes.buttons}>
-        <Button variant='contained' onClick={resetCache}>Reset all activities and lists</Button>
+        <Button variant='contained' onClick={() => setDialogOpen(true)}>
+          Reset all chains and activities
+        </Button>
       </Container>
+
+      {
+        showAlert
+          ? <Alert message={'Erased all changes'} hideAlert={() => setShowAlert(false)} />
+          : <></>
+      }
+
+      <ResetConfirmationDialog 
+        open={dialogOpen} 
+        action={resetCache}
+        cancel={() => setDialogOpen(false)}
+      />
     </div>
   )
 }
